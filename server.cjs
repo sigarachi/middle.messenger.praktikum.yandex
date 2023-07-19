8d2ee3b42b6f971a3c251ca484e0a41f02237fb0
sigarachi <lalalabtv@gmail.com~>
2023-07-19 10:23:50 +0300

    Test  deploy

--- server.cjs
+++ server.cjs
@@ -1,17 +1,24 @@
+'use strict'
 const express = require('express');
 const path = require('path');
+const serverless = require('serverless-http');
 const PORT = 3000;
 const app = express();
-
-
+const router = express.Router();
 
 app.use(express.static(path.resolve(__dirname, 'dist'), {extensions: ["js"]}));
+app.set('view engine', 'pug');
 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
+app.use('/.netlify/functions/server', router);
+
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}`)
 })
+
+module.exports = app;
+module.exports.handler = serverless(app);
